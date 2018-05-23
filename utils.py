f587a50ae9c0c4bdb3f688b2e6b417dd79cae28f
lpcinelli <lpcinelli@gmail.com>
2018-05-23 14:49:25 -0300

    Adds History log class and helper for kwargs cmd parser
    Introduces History class which contains a dict of dicts: a dict for
    each mode (usually: 'train', 'val', 'test'). Adapts NumpyEncoder for
    History class.
    Adds parser funtion parse_kwparams for more complex command-line optional
    params.

--- utils.py
+++ utils.py
@@ -4,10 +4,50 @@ import pickle
 import numpy as np
 
 
+class History(object):
+    def __init__(self):
+        self.history = {}
+
+    def update(self, name, value, mode=None):
+        if isinstance(value, dict):
+            self.history[mode][name] = value
+            return
+        try:
+            self.history[mode][name] += [value]
+        except KeyError:
+            try:
+                self.history[mode][name] = [value]
+            except KeyError:
+                self.history[mode] = {name: [value]}
+
+    def averages(self, weights_key, exclude=[]):
+        exclude += [weights_key]
+        for mode, history in self.history.items():
+            weights = self.history[mode].get(weights_key)
+            weights = [len(frames) for frames in weights]
+            for name, meter in history.items():
+                if name in exclude:
+                    continue
+                self.history[mode][name] += [
+                    np.average(meter, weights=weights, axis=0)]
+
+
+def print_result(history, title, exclude=[]):
+    # print('\n{}'.format(title))
+    msg = ['\n{}\n'.format(title)]
+    msg += ['{0}: {1}\n'.format(name, meter[-1]) for name, meter in
+            history.items() if name not in exclude]
+
+    print(''.join(msg))
+
+
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
+        if isinstance(obj, History):
+            print('got inside Hisotry objj decoder numpy')
+            return obj.history
         return json.JSONEncoder.default(self, obj)
 
 
@@ -18,3 +58,24 @@ def save_data(data, filename, json_format=True, pickle_format=True):
     if pickle_format:
         with open(filename + '.pkl', 'wb') as fp:
             pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def parse_kwparams(kwlst):
+    '''
+    Parses key-worded parameters.
+
+    @param kwstr key-worded parameters list to be parsed.
+
+    @return dictionary with the key-worded parameters.
+    '''
+
+    # Set in dictionary form
+    kwparams = {}
+    for param in kwlst:
+        k, v = param.split('=')
+        try:
+            kwparams[k] = json.loads(v)
+        except json.JSONDecodeError:
+            kwparams[k] = v
+    return kwparams
+
